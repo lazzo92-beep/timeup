25a9f226caab22ea279d544dd1e4d19d2629e8d5
lazzo92-beep <lazzo92@gmail.com>
2026-07-10 15:06:20 +0200

    Configure Trello Power-Up key

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -7,9 +7,8 @@ import { APP_INFO } from "../utils/constants.js";
 
 export const AppConfig = {
   // Trello Power-Up API Key
-  // Replace this with the API key from your Trello Power-Up Admin page.
   // This is the public identifier for the app. It is safe to expose in client-side code.
-  APP_KEY: "REPLACE_WITH_YOUR_TRELLO_POWER_UP_KEY",
+  APP_KEY: "aa778b9ec659bc02679d3e28d6e0b92e",
 
   // Application Name
   APP_NAME: APP_INFO.POWER_UP_NAME,
